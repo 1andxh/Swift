176827d60170a224e963c32aa9d22307009af0b9
Rachel Mawufemor Cudjoe Abede <90472602+Femorr@users.noreply.github.com>
2023-07-26 11:07:26 +0000

    Add files via upload
    final commit

---
+++ src/sample/Main.java
@@ -0,0 +1,42 @@
+/**
+ * group name : SWIFT
+ * ---members ID---
+ * 10888653
+ * 10908662
+ * 10893106
+ * 10867351
+ * 10869753
+ */
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.Date;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("registration.fxml"));
+        primaryStage.setTitle("Pharmacy Management System");
+        primaryStage.resizableProperty().set(false);
+        primaryStage.setScene(new Scene(root, 750, 600));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+        primaryStage.show();
+        }catch (Exception e){
+            System.out.println("start");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
